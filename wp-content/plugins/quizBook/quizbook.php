7fe2185453ef789a1717fb6b933bdc24e5cf5ff7
Bruno Barros <contato4code@gmail.com>
2022-09-28 11:04:30 -0300

    Workflow de registro do plugin e reescrita do htaccess

--- wp-content/plugins/quizBook/quizbook.php
+++ wp-content/plugins/quizBook/quizbook.php
@@ -10,3 +10,13 @@ License: GPL2
 License URI: www.gnu.org/licences/gpl-2.0.html
 Text Domain: quizbook
  */
+
+require_once plugin_dir_path(__FILE__) . 'includes/postypes.php';
+
+
+/*
+ * 
+ *  Recreate regras de URL (htaccess)
+ * 
+ */
+register_activation_hook(__FILE__, 'rewrite_flush');
